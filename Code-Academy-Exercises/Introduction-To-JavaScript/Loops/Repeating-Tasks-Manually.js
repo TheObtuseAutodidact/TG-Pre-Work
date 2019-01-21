a882fa1cb0311bf808b40cf4bf3b75965aa1db90
Obtuse <theobtuseautodidact@gmail.com>
2019-01-21 16:54:44 -0700

    emo of why we need loops

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,9 @@
+// Write your code below
+let vacationSpots = ['one', 'two', 'three'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+
+// Demo of why we need loops
