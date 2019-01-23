b60113703be590623a0466b148f46edb2e87ee4c
Obtuse <theobtuseautodidact@gmail.com>
2019-01-23 13:19:28 -0700

    Naming individual exports

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Named-exports.js
@@ -0,0 +1,25 @@
+
+
+let availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800,
+  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators']
+ },
+ {name: 'SkyJet',
+  fuelCapacity: 500,
+  availableStaff: ['pilots', 'flightAttendants']
+ }
+];
+
+let flightRequirements = {
+  requiredStaff: 4,
+};
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+  return availableStaff.length >= requiredStaff;
+};
+
+export { availableAirplanes, flightRequirements, meetsStaffRequirements };
+
+// Naming individual exports
