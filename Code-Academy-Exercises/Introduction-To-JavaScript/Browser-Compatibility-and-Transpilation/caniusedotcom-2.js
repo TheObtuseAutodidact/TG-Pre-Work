ec452212e58893e61f71a7aea376df4068ca020b
Obtuse <theobtuseautodidact@gmail.com>
2019-01-23 11:36:07 -0700

    Support for ES5 v ES6 according to caniuse.com

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,7 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 95.38;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 87.83;
+
+// Support for ES5 v ES6 according to caniuse.com
