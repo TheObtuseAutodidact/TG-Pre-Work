5b5aa1bd43506dc9aa82f398dfd3cd70067d4df8
Obtuse <theobtuseautodidact@gmail.com>
2019-01-23 09:22:28 -0700

    Intro to getters

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Getters.js
@@ -0,0 +1,15 @@
+_energyLevelconst robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel(){
+    if(typeof this._energyLevel === 'number') {
+      return 'My current energy level is ' + this._energyLevel
+    } else {
+      return "System malfunction: cannot retrieve energy level"
+    }
+  }
+};
+
+console.log(robot.energyLevel);
+
+// Intro to getters
